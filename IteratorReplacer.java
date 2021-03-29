dd62029b1e27a19c6ba5b62d174fcc83503a6752
600020344 <77069279+600020344@users.noreply.github.com>
2021-03-29 10:31:50 -0700

    Update IteratorReplacer.java

--- IteratorReplacer.java
+++ IteratorReplacer.java
@@ -13,14 +13,28 @@ public class IteratorReplacer
 
 	public IteratorReplacer(String line, String rem, String rep)
 	{
+    String[] words = line.split(" ");   
+    list = new ArrayList<String>(Arrays.asList(words));
+    toRemove = rem;
+    replaceWith = rep;
 	}
 
 	public void setEmAll(String line, String rem, String rep)
 	{
+    String[] words = line.split(" ");   
+    list = new ArrayList<String>(Arrays.asList(words));
+    toRemove = rem;
+    replaceWith = rep;
 	}
 
 	public void replace()
 	{
+    ListIterator i = list.listIterator();
+    while (i.hasNext())
+    {
+      if (i.next().equals(toRemove))
+        i.set(replaceWith);
+    }
 	}
 
 	public String toString()
